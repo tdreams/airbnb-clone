05b2f8c6e0606a4b1788e89d2ca014e7594195dc
tdreams <fabriels@gmail.com>
2023-08-22 16:50:27 -0700

    button

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,6 +2,8 @@
 
 import { cn } from "@/lib/utils";
 import { IconType } from "react-icons";
+import MouseBlurEffect from "./MouseBlurEffect";
+import { useRef, useState } from "react";
 
 interface ButtonProps {
   label: string;
@@ -20,24 +22,36 @@ const Button = ({
   small,
   icon: Icon,
 }: ButtonProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
   return (
-    <button
-      className={cn(
-        " relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition  w-full",
-        outline ? "bg-white" : "bg-gradient-to-r from-rose-400 to-rose-700",
-        outline ? "border-black" : "bg-gradient-to-r from-rose-400 to-rose-700",
-        outline ? "text-black" : "text-white",
-        small ? "text-sm" : "text-base",
-        small ? "p-1" : "py-3",
-        small ? "font-light" : "font-semibold",
-        small ? "border-[1px]" : "border-2"
-      )}
-      onClick={onClick}
-      disabled={disabled}
+    <div
+      className="relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      {Icon && <Icon size={24} className="absolute left-4 top-3" />}
-      {label}
-    </button>
+      <button
+        ref={buttonRef}
+        className={cn(
+          " relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg  transition  w-full overflow-hidden",
+          outline ? "bg-white" : "bg-gradient-to-r from-rose-400 to-rose-700",
+          outline
+            ? "border-black"
+            : "bg-gradient-to-r from-rose-400 to-rose-700",
+          outline ? "text-black" : "text-white",
+          small ? "text-sm" : "text-base",
+          small ? "p-1" : "py-3",
+          small ? "font-light" : "font-semibold",
+          small ? "border-[1px]" : "border-2"
+        )}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        <MouseBlurEffect isShown={isHovered} buttonRef={buttonRef} />
+        {Icon && <Icon size={24} className="absolute left-4 top-3" />}
+        {label}
+      </button>
+    </div>
   );
 };
 
---
+++ components/MouseBlurEffect.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { motion, useMotionValue } from "framer-motion";
+
+interface MouseBlurEffectProps {
+  isShown: boolean;
+  buttonRef: React.RefObject<HTMLButtonElement>;
+}
+
+const MouseBlurEffect: React.FC<MouseBlurEffectProps> = ({
+  isShown,
+  buttonRef,
+}) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (buttonRef.current) {
+        x.set(event.clientX - buttonRef.current.offsetLeft);
+        y.set(event.clientY - buttonRef.current.offsetTop);
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [x, y, buttonRef]);
+
+  useEffect(() => {
+    let hideTimer: number;
+
+    if (!isShown) {
+      hideTimer = window.setTimeout(() => {
+        setIsHidden(true);
+      }, 3000); // Adjust the delay as needed
+    }
+
+    return () => {
+      clearTimeout(hideTimer);
+    };
+  }, [isShown, isHidden]);
+
+  return (
+    <>
+      {isShown && buttonRef.current && (
+        <motion.div
+          className="fixed z-50 lg:block duration-75 ease-in-out transition bg-gradient-to-l from-rose-700 to-rose-800"
+          style={{
+            width: buttonRef.current.clientWidth * 1.5,
+            height: buttonRef.current.clientHeight,
+            borderRadius: "80%",
+            pointerEvents: "none",
+            filter: "blur(25px)",
+            mixBlendMode: "overlay",
+            opacity: 0.9,
+            position: "fixed",
+            left: buttonRef.current.offsetLeft,
+            top: buttonRef.current.offsetTop,
+            translateX: x,
+            translateY: y,
+            transition: "transform 0.3s ease-out",
+          }}
+        ></motion.div>
+      )}
+    </>
+  );
+};
+
+export default MouseBlurEffect;
